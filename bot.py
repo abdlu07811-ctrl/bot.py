cc940d863e95d5f6c6e50825a233b0993ce512a4
abdlu07811-ctrl <abdlu07811@gmail.com>
2026-05-24 04:21:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-first upload
+from instagrapi import Client
+
+cl = Client()
+
+username = "OS.wd"
+password = "07823520Aa"
+
+cl.login(username, password)
+
+cl.dump_settings("instagram_session.json")
+
+print("✅ تم تسجيل الدخول")
